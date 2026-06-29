26a34df3c53690981fcb55d197f4fd20c7ecc2f7
Ahsan Ahmed <ahsan.ahmed3246@gmail.com>
2026-06-29 14:53:05 -0700

    two way datalink support

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -6,13 +6,26 @@
 #include "StateEstimator.h"
 
 // Must be the same on GCS
+enum States {
+	IDLE,
+	TELEOP,
+	AUTON,
+};
+
 typedef struct {
 	size_t packet_number;
 	float missionTime;
+	States state;
 	float gx, gy, gz, ax, ay, az; // imu data
 	float roll, pitch, yaw; // orientation
 	float x, y, z; // location
 } TelemetryPacket;
+
+typedef struct {
+	States state;
+	float thrust, roll, pitch, yaw; // if teleop
+	float xpos, ypos, zpos; // if auton
+} Command;
 
 uint8_t broadcastAddress[] = {0xFC, 0x01, 0x2C, 0xD0, 0x77, 0x5C};
 
@@ -23,6 +36,12 @@ MPU6050Data i;
 uint32_t startTime = 0;
 uint32_t lastTime = 0;
 
+
+Command currentCommand = {IDLE, 0,0,0,0,0,0,0};
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+	memcpy(&currentCommand, incomingData, sizeof(currentCommand));
+}
 
 void setup () {
 	Wire.begin(6, 7);
@@ -36,6 +55,7 @@ void setup () {
 
 	WiFi.mode(WIFI_STA);
 	esp_now_init();
+	esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 
 	esp_now_peer_info_t peerInfo = {};
 	memcpy(peerInfo.peer_addr, broadcastAddress, 6);
@@ -64,6 +84,7 @@ void loop () {
 		TelemetryPacket telem = {
 			packet_number,
 			t,
+			currentCommand.state,
 			i.gx,
 			i.gy,
 			i.gz,
